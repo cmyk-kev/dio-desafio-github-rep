44d2cfe6d13cfb7e470ca7e6fec5a3a8b85cf755
Kevin da Silva Felix <kevinsfelix@outlook.com>
2021-12-02 21:27:03 -0300

    pasta de aula referente a dotnet e C#

---
+++ C#/DigitalInnovationOne/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberOfTimes = 5;
+
+for (int i = 0; i < numberOfTimes; i++)
+{
+  Console.WriteLine($"Welcome to the course of .NET {i}");
+}
